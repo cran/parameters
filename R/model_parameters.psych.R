7a53e8cfb5fd96d4f61c40cb203fd833626a109f
Daniel Lüdecke <d.luedecke@uke.de>
2020-01-14 20:30:02 +0000

    version 0.4.0

--- R/model_parameters.psych.R
+++ R/model_parameters.psych.R
@@ -10,8 +10,9 @@
 #' @details
 #'  For the structural models obtained with \pkg{psych}, the following indices are present:
 #'  \itemize{
-#'    \item \strong{Complexity} (Hoffman's, 1978; Pettersson and Turkheimer, 2010) represents the number of latent components needed to account for the observed variables. Whereas a perfect simple structure solution has a complexity of 1 in that each item would only load on one factor, a solution with evenly distributed items has a complexity greater than 1.
+#'    \item \strong{Complexity} (\cite{Hoffman's, 1978; Pettersson and Turkheimer, 2010}) represents the number of latent components needed to account for the observed variables. Whereas a perfect simple structure solution has a complexity of 1 in that each item would only load on one factor, a solution with evenly distributed items has a complexity greater than 1.
 #'    \item \strong{Uniqueness} represents the variance that is 'unique' to the variable and not shared with other variables. It is equal to \code{1 – communality} (variance that is shared with other variables). A uniqueness of \code{0.20} suggests that 20\% or that variable's variance is not shared with other variables in the overall factor model. The greater 'uniqueness' the lower the relevance of the variable in the factor model.
+#'    \item \strong{MSA} represents the Kaiser-Meyer-Olkin Measure of Sampling Adequacy (\cite{Kaiser and Rice, 1974}) for each item. It indicates whether there is enough data for each factor give reliable results for the PCA. The value should be > 0.6, and desirable values are > 0.8 (\cite{Tabachnick and Fidell, 2013}).
 #' }
 #'
 #' @examples
@@ -29,7 +30,15 @@
 #' \donttest{
 #' # Exploratory Factor Analysis (EFA) ---------
 #' efa <- psych::fa(attitude, nfactors = 3)
-#' model_parameters(efa, threshold = "max", sort = TRUE, labels = as.character(1:ncol(attitude)))}
+#' model_parameters(efa, threshold = "max", sort = TRUE, labels = as.character(1:ncol(attitude)))
+#' }
+#'
+#' # Omega ---------
+#' omega <- psych::omega(mtcars, nfactors = 3)
+#' params <- model_parameters(omega)
+#' params
+#' summary(params)
+#'
 #'
 #' # FactoMineR ---------
 #' \dontrun{
@@ -40,11 +49,14 @@
 #' attributes(model_parameters(model))$scores
 #'
 #' model <- FactoMineR::FAMD(iris, ncp = 2)
-#' model_parameters(model)}
-#' @return A data.frame of loadings.
+#' model_parameters(model)
+#' }
+#' @return A data frame of loadings.
 #' @references \itemize{
+#'   \item Kaiser, H.F. and Rice. J. (1974). Little jiffy, mark iv. Educational and Psychological Measurement, 34(1):111–117
 #'   \item Pettersson, E., \& Turkheimer, E. (2010). Item selection, evaluation, and simple structure in personality data. Journal of research in personality, 44(4), 407-420.
 #'   \item Revelle, W. (2016). How To: Use the psych package for Factor Analysis and data reduction.
+#'   \item Tabachnick, B. G., and Fidell, L. S. (2013). Using multivariate statistics (6th ed.). Boston: Pearson Education.
 #' }
 #' @export
 model_parameters.principal <- function(model, sort = FALSE, threshold = NULL, labels = NULL, ...) {
@@ -89,6 +101,7 @@ model_parameters.principal <- function(model, sort = FALSE, threshold = NULL, la
   # Add information
   loadings$Complexity <- model$complexity
   loadings$Uniqueness <- model$uniquenesses
+  loadings$MSA <- attributes(model)$MSA
 
   # Add attributes
   attr(loadings, "summary") <- data_summary
@@ -126,6 +139,8 @@ model_parameters.principal <- function(model, sort = FALSE, threshold = NULL, la
 
 
 
+
+
 #' @export
 model_parameters.fa <- model_parameters.principal
 
@@ -134,16 +149,29 @@ model_parameters.fa <- model_parameters.principal
 
 #' @rdname model_parameters.principal
 #' @export
-model_parameters.omega <- function(model, sort = FALSE, threshold = NULL, labels = NULL, ...) {
+model_parameters.omega <- function(model, ...) {
 
-  # TODO: TO BE SERVERLY IMPROVED
-  # https://github.com/cran/psych/blob/master/R/print.psych.omega.R
+  # Table of omega coefficients
+  table_om <- model$omega.group
+  colnames(table_om) <- c("Omega_Total", "Omega_Hierarchical", "Omega_Group")
+  table_om$Composite <- row.names(table_om)
+  row.names(table_om) <- NULL
+  table_om <- table_om[c("Composite", names(table_om)[names(table_om) != "Composite"])]
 
-  # Get loadings
-  loadings <- as.data.frame(unclass(model$schmid$sl))
+  # Get summary: Table of Variance
+  table_var <- as.data.frame(unclass(model$omega.group))
+  table_var$Composite <- rownames(model$omega.group)
+  table_var$Total <- table_var$total * 100
+  table_var$General <- table_var$general * 100
+  table_var$Group <- table_var$group * 100
+  table_var <- table_var[c("Composite", "Total", "General", "Group")]
 
-  # Format
-  loadings <- cbind(data.frame(Variable = row.names(loadings)), loadings)
-  row.names(loadings) <- NULL
-  loadings
+  # colnames(table_var) <- c("Composite", "Total Variance (%)", "Variance due to General Factor (%)", "Variance due to Group Factor (%)")
+
+  # cor.plot(psych::fa.sort(om), main = title)
+
+  out <- table_om
+  attr(out, "summary") <- table_var
+  class(out) <- c("parameters_omega", class(out))
+  out
 }
